3d0c0c19b537d9ebf766ca0fc9d5bcb475ddd950
Moses-Maina-ctrl <mosesmaina1245@gmail.com>
2024-06-20 23:41:38 +0300

    Update wagon struct by adding wallet and career variables

--- oregon.c
+++ oregon.c
@@ -1,37 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-//TODO rename MainCharacter to Wagon
+//TODO rename MainCharacter
 typedef struct {
   char *name;
   char *career;
-} MainCharacter;
+  int wallet;
+} wagon;
+
 
 void chooseCharacter(void)
 {
   char name[20];
-  char career[20];
-  char moveChar;
+  int moveChar;
 
-  MainCharacter playerOne;
   printf("Many kinds of people made the trip to Oregon.\n You may:\n \t 1. Be a banker from Boston\n \t 2. Be a Carpenter from Ohio\n \t 3. Be a farmer from Illinois\n \t 4. Find out the differences between these choices\nWhat is your choice? ");
+  scanf("%d",&moveChar); //scanf takes the format argument and a pointer of the variable and not the variable itself
+  printf("%d\n",moveChar);
 
-  if (moveChar == "1")
+  if (moveChar == 1)
   {
-    playerOne.career = "banker";
+    wagon player = {.career="banker", .wallet=1600};
+    printf("wagon: %s  wallet=$ %d",player.career, player.wallet);
   }
-  else if (moveChar == "2")
+  else if (moveChar == 2)
   {
-    playerOne.career = "carpenter";
+    wagon player = {.career="carpenter", .wallet=800};
+    printf("wagon: %s  wallet=$ %d",player.career, player.wallet);
   }
-  else if(moveChar == "3")
+  else if(moveChar == 3)
   {
-    playerOne.career = "farmer";
+    wagon player = {.career="farmer", .wallet=400};
+    printf("wagon: %s  wallet=$ %d",player.career, player.wallet);
   }
-  else if (moveChar == "4")
+  else if (moveChar == 4)
   {
     printf("Travelling to Oregon isn't easy!\nBut if you're a banker, you'll have more money for supplies and services than a farmer or a carpenter");
     printf("\n");
     printf("However, the harder you have to try, the more points you deserve!\n Therefore, the farmer earns the greatest number of points and the banker earns the least");
+  } else{
+    printf("Wrong input, try again");
+    chooseCharacter();
   }
 }
 
@@ -48,10 +56,11 @@ int main(void)
 {
   char c;
   printf("Welcome to The Oregon Trail\n You may: (Enter Number)\n \t 1. Travel the trail \n \t 2. Learn about the trail \n \t 3. Quit\n");
-  c = getchar();
+  scanf("%c",&c);
   int move = c - '0';
   switch (move){
     case 1:
+      chooseCharacter();
       break;
     case 2:
       learnMore();
